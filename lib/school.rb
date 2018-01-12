7b44a22711453fca4530529b5f7f0dd1a011f70c
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-01-12 13:38:43 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,7 +11,7 @@ class School
 
 
   def add_student(name, grade)
-    if @roster[grade] = nil
+    if @roster[grade].length  = 0
       @roster[grade] = [name]
     else
       @roster[grade] << [name]
